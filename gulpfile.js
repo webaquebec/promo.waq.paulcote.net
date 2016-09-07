4e0c3131d521989bd2efd9118b18de86d7730567
Paul Côté <github@paulcote.net>
2016-09-06 22:39:54 -0400

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -10,5 +10,5 @@ requireDir('./gulp/tasks', { recurse: true });
 gulp.task('build', ['bower', 'sass', 'copy', 'concat:helpers', 'concat:templates', 'svg2png', 'svgSprite', 'twig']);
 gulp.task('export', ['bower:export', 'sass:export', 'copy:export', 'concat:helpers:export', 'svg2png:export', 'svgSprite:export']);
 gulp.task('default', function() {
-    runSequence('clean', 'build', 'symlink', 'watch');
+    runSequence('clean', 'build', 'watch');
 });
