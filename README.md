32f0e1f243b326a5227706f89e7b2c4b9af021b4
Paul Côté <github@paulcote.net>
2016-09-17 16:46:17 -0400

    Lots of edits

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# VTT - Starter-kit
+# Web à Québec 2017 - Promo page
 
-VTT (Vanilla Templating Tools) est constitué d'une banque d'outils de développement Web visant à faciliter et augmenter la productivité d'un projet Web.
-
-Après un survol général de ce que le Web peut nous offrir en terme de plugins, de framework et autres widgets, nous sommes venus à la conclusion qu'il était mieux de développer ensemble notre propre tool kit pour des raisons telles que le respect des bonnes pratiques, l'uniformité du code et l'accessibilité Web.
-
-VTT est la somme revue et pauffinnée de tous nos morceaux d'expertise en intégration chez Libéo.
+Complete readme to come soon, bit in a rush there...
--- src/js/main.js
+++ src/js/main.js
@@ -11,78 +11,136 @@
 
 $(function() {
 
-    $('.nav-next').on('click', function(){
-        var current = $('.is-active');
-        var befores = $('[class*=pos-bf]').sort(sortMe);
-        var afters = $($('[class*=pos-af]').sort(sortMe).get().reverse());
+    var startX,
+        startY,
+        dist,
+        threshold = 50, //required min distance traveled to be considered swipe
+        minTime = 100, // maximum time allowed to travel that distance
+        maxTime = 750, // maximum time allowed to travel that distance
+        elapsedTime,
+        startTime;
 
-        current.removeClass('is-active').addClass('pos-bf-1');
+    $('.alum').on('touchstart', function(e) {
+        var touchobj = e.changedTouches[0]
+        dist = 0
+        startX = touchobj.pageX
+        startY = touchobj.pageY
+        startTime = new Date().getTime() // record time when finger first makes contact with surface
+    })
 
-        $.each(befores, function( index, item ) {
-            var pos = parseInt(item.className.replace('person','').replace('pos-bf-','').replace('pos-af-',''));
-            $(item).removeClass(function(index, css) {
-                return (css.match(/\bpos-bf-\S+/g) || []).join(' ');
-            })
-            if(index == 0){
-                $(item).addClass('pos-af-'+afters.length);
+    $('.alum').on('touchend', function(e) {
+        var touchobj = e.changedTouches[0]
+        dist = touchobj.pageX - startX // get total dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime // get time elapsed
+        if(elapsedTime>minTime && elapsedTime<maxTime && Math.abs(dist) >= threshold){
+            var swiperightBol = dist>=0;
+            if(swiperightBol){
+                moveRight();
             }
-            else{
-                $(item).addClass('pos-bf-'+(pos+1));
+            else {
+                moveLeft();
             }
-        });
 
-        $.each(afters, function( index, item ) {
-            var pos = parseInt(item.className.replace('person','').replace('pos-bf-','').replace('pos-af-',''));
-            $(item).removeClass(function(index, css) {
-                return (css.match(/\bpos-af-\S+/g) || []).join(' ');
-            })
-            if(index == 0){
-                $(item).addClass('is-active');
-            }
-            else{
-                $(item).addClass('pos-af-'+(pos-1));
-            }
-        });
-    });
+            e.preventDefault();
+        }
+    })
 
-    $('.nav-prev').on('click', function(){
-        var current = $('.is-active');
-        var afters = $('[class*=pos-af]').sort(sortMe);
-        var befores = $($('[class*=pos-bf]').sort(sortMe).get().reverse());
+    $('.nav-next').on('click', moveLeft);
+    $('.nav-prev').on('click', moveRight);
 
-        current.removeClass('is-active').addClass('pos-af-1');
-
-        $.each(befores, function( index, item ) {
-            var pos = parseInt(item.className.replace('person','').replace('pos-bf-','').replace('pos-af-',''));
-            $(item).removeClass(function(index, css) {
-                return (css.match(/\bpos-bf-\S+/g) || []).join(' ');
-            })
-            if(index == 0){
-                $(item).addClass('is-active');
-            }
-            else{
-                $(item).addClass('pos-bf-'+(pos-1));
-            }
-        });
-
-        $.each(afters, function( index, item ) {
-            var pos = parseInt(item.className.replace('person','').replace('pos-bf-','').replace('pos-af-',''));
-            $(item).removeClass(function(index, css) {
-                return (css.match(/\bpos-af-\S+/g) || []).join(' ');
-            })
-            if(index == 0){
-                $(item).addClass('pos-bf-'+afters.length);
-            }
-            else{
-                $(item).addClass('pos-af-'+(pos+1));
-            }
-        });
+    $('.person').on('touchend', function(){
+        if($(this).hasClass('is-active')){
+            $(this).toggleClass('is-hover');
+        }
     });
 
 });
 
 function sortMe(a, b) {
-    var aPos = a.className.replace('person','').replace('pos-bf-','').replace('pos-af-','');
-    var bPos = b.className.replace('person','').replace('pos-bf-','').replace('pos-af-','');
-    return aPos < bPos;
+    var aPos = parseInt(a.className.replace('person', '').replace('pos-bf-', '').replace('pos-af-', ''));
+    var bPos = parseInt(b.className.replace('person', '').replace('pos-bf-', '').replace('pos-af-', ''));
+    return bPos - aPos;
+}
+
+var animating = false;
+function moveLeft(){
+    if(!animating){
+        animating = true;
+        var current = $('.is-active');
+        var befores = $('[class*=pos-bf]').sort(sortMe);
+        var afters = $($('[class*=pos-af]').sort(sortMe).get().reverse());
+        $('.person.is-hover').removeClass('is-hover');
+
+        console.log(befores);
+        console.log(afters);
+
+        current.removeClass('is-active').addClass('pos-bf-1');
+
+        $.each(befores, function(index, item) {
+            var pos = parseInt(item.className.replace('person', '').replace('pos-bf-', '').replace('pos-af-', ''));
+            $(item).removeClass(function(index, css) {
+                return (css.match(/\bpos-bf-\S+/g) || []).join(' ');
+            })
+            if (index == 0) {
+                $(item).addClass('pos-af-' + afters.length);
+            } else {
+                $(item).addClass('pos-bf-' + (pos + 1));
+            }
+        });
+
+        $.each(afters, function(index, item) {
+            var pos = parseInt(item.className.replace('person', '').replace('pos-bf-', '').replace('pos-af-', ''));
+            $(item).removeClass(function(index, css) {
+                return (css.match(/\bpos-af-\S+/g) || []).join(' ');
+            })
+            if (index == 0) {
+                $(item).addClass('is-active');
+            } else {
+                $(item).addClass('pos-af-' + (pos - 1));
+            }
+        });
+
+        setTimeout(function(){animating=false;},500);
+    }
+}
+
+function moveRight() {
+    if(!animating){
+        animating = true;
+        var current = $('.is-active');
+        var afters = $('[class*=pos-af]').sort(sortMe);
+        var befores = $($('[class*=pos-bf]').sort(sortMe).get().reverse());
+        $('.person.is-hover').removeClass('is-hover');
+
+        console.log(befores);
+        console.log(afters);
+
+        current.removeClass('is-active').addClass('pos-af-1');
+
+        $.each(befores, function(index, item) {
+            var pos = parseInt(item.className.replace('person', '').replace('pos-bf-', '').replace('pos-af-', ''));
+            $(item).removeClass(function(index, css) {
+                return (css.match(/\bpos-bf-\S+/g) || []).join(' ');
+            })
+            if (index == 0) {
+                $(item).addClass('is-active');
+            } else {
+                $(item).addClass('pos-bf-' + (pos - 1));
+            }
+        });
+
+        $.each(afters, function(index, item) {
+            var pos = parseInt(item.className.replace('person', '').replace('pos-bf-', '').replace('pos-af-', ''));
+            $(item).removeClass(function(index, css) {
+                return (css.match(/\bpos-af-\S+/g) || []).join(' ');
+            })
+            if (index == 0) {
+                $(item).addClass('pos-bf-' + afters.length);
+            } else {
+                $(item).addClass('pos-af-' + (pos + 1));
+            }
+        });
+
+        setTimeout(function(){animating=false;},500);
+    }
 }
